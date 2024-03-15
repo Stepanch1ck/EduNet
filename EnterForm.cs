6e87d541f69655d1cfc51739d90c963acf6f671f
Stepan Ch <noname.churkin@gmail.com>
2024-03-15 11:22:54 +0300

    добавил базу данных LogPass

---
+++ EnterForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+
+namespace EduNet
+{
+    public partial class EnterForm : Form
+    {
+        private SqlConnection sqlconn;
+
+        public EnterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterForm_Load(object sender, EventArgs e)
+        {
+            sqlconn = new SqlConnection(ConfigurationManager.ConnectionStrings["EduDB"].ConnectionString );
+            sqlconn.Open();
+            if (sqlconn.State == ConnectionState.Open) 
+            {
+                MessageBox.Show("Подключение установленно");
+            }
+        }
+    }
+     
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace EduNet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new EnterForm());
         }
     }
 }
